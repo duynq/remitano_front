eaa1f414fc98293e466be45811dd43c48720bc61
duynq <quangduyx188@gmail.com>
2024-04-17 15:00:34 +0700

    fix socket

--- src/channels/socket.ts
+++ src/channels/socket.ts
@@ -1,7 +1,7 @@
 import { createConsumer } from '@rails/actioncable'
 
 const socket = (token: string) => {
-  return createConsumer(`ws://localhost:3000/cable?token=${token}`)
+  return createConsumer(`ws://obscure-beyond-24006-9ba56bd5d8e2.herokuapp.com/cable?token=${token}`)
 }
 
 export default socket
